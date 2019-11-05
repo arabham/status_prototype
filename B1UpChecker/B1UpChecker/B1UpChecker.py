9997ff1300000b98e7f7c520313710b5350eed62
Abraham Hoogendoorn <arabham110@gmail.com>
2019-11-04 21:06:52 -0500

    got rig name working. rig status partial. rest only framework done.

--- B1UpChecker/B1UpChecker/B1UpChecker.py
+++ B1UpChecker/B1UpChecker/B1UpChecker.py
@@ -8,16 +8,9 @@ from queue import Queue
 
 active = []
 cnopts = pysftp.CnOpts()
-rhostname = '192.168.1.15'
 rusername = 'root'
 rpassword = 'arabham110@gmail.com'
 cnopts.hostkeys = None  
-
-with pysftp.Connection(host=rhostname, username=rusername, password=rpassword, cnopts=cnopts) as sftp:
-    print("Connection successfully astablished ...")
-    remoteFilePath = '/home/miner/config.json'
-    localFilePath = './screen.miner.log'
-    sftp.get(remoteFilePath, localFilePath)
 
 class Rig:
 
@@ -58,35 +51,9 @@ class Rig:
     def getRigIP(self):
         return(self.ip)
 
-def vlan1Scan():
-    lock=threading.Lock()
-    _start=time.time()
-    def check(n):
-        with open(os.devnull, "wb") as limbo:
-                ip="192.168.1.{0}".format(n)
-                result=subprocess.Popen(["ping", "-n", "1", "-w", "300", ip],stdout=limbo, stderr=limbo).wait()
-                with lock:                    
-                    if not result:
-                        active.append(ip)
-                        print (ip, "active")                    
-                    else:
-                        #print (ip, "inactive") 
-                        pass
-    def threader():
-        while True:
-            worker=q.get()
-            check(worker)
-            q.task_done()
-    q=Queue()
-
-    for x in range(255):
-        t=threading.Thread(target=threader)
-        t.daemon=True
-        t.start()
-    for worker in range(1,255):
-        q.put(worker)
-    q.join()
-    print("Process completed in: ",time.time()-_start)
+    def getRigAll(self):
+        rigAll = [self.name,self.status,self.hashrate,self.temp,self.ip]
+        return(rigAll)
 
 def vlan2Scan():
     lock=threading.Lock()
@@ -307,28 +274,86 @@ def vlan8Scan():
 
 def runVlanScans():
     global active
-    vlan1Scan(),vlan2Scan(),vlan3Scan(),vlan4Scan(),vlan5Scan(),vlan6Scan(),vlan7Scan(),vlan8Scan()
+    vlan2Scan(),vlan3Scan(),vlan4Scan(),vlan5Scan(),vlan6Scan(),vlan7Scan(),vlan8Scan()
     return active
 
-def parseLog():
+def parseRigName():
+    rigName = ''
+    data = []
+    with open("name.miner.log", "r") as f:
+        for line in f:
+            for word in line.split('"'):
+                data.append(word)
+
+    for i, x in enumerate(data):
+       if(x.startswith('AAB1')):
+           rigName = x
+    print(rigName)
+    return rigName
+
+def parseRigStatus():
     loglines = []
-    with open ('screen.miner.log','rt') as screenminerlog:
+    rigStatus = 'Online'
+    with open ('screen.miner.log', 'r') as screenminerlog:
         for logline in screenminerlog:
             loglines.append(logline)
-        print(loglines[0])
-        #loglines.reverse()
+
+    loglines.reverse()
 
     for num, lines in enumerate(loglines):
-        if(loglines[num][0] == 'G'):
-            print('RIGHT!')
-            print(loglines[num])
+        if(loglines[num][0] != 'G'):
+            rigStatus = 'Troubleshoot'
             break
-    
+
+    return rigStatus
+
+def parseRigHashRate():
+    return "Needs work"
+
+def parseRigTemp():
+    return "Needs work"
+
+def ftpLogs(rigIP):
+    try:
+        with pysftp.Connection(host=rigIP, username=rusername, password=rpassword, cnopts=cnopts) as sftp:
+            remoteNameFilePath = '/home/miner/config.json'
+            remoteFilePath = '/var/tmp/screen.miner.log'
+            localNameFilePath = './name.miner.log'
+            localFilePath = './screen.miner.log'
+
+            if exists(remoteNameFilePath):
+                if exists(localNameFilePath):
+                    sftp.get(remoteNameFilePath, localNameFilePath)
+                else:
+                    print("local file path does not exist!")
+            else:
+                print("remote file path does not exist!")
+
+            if exists(remoteFilePath):
+                if exists(localFilePath):
+                    sftp.get(remoteFilePath, localFilePath)
+                else:
+                    print("local file path does not exist!")
+            else:
+                print("remote file path does not exist!")
+    except:
+        print("failed to connect to server!")
+
+
 if __name__ == '__main__':
     # active ip scan
     runVlanScans();
+    global rig
     # TODO remove
     print(len(active))
+    for i, x in enumerate(active):
+        rigIP = x
+        ftpLogs(rigIP)
+        rigName = parseRigName()
+        rigStatus = parseRigStatus()
+        rigHashrate = parseRigHashRate()
+        rigTemp = parseRigTemp()
+        rig = Rig(rigName,rigStatus,rigHashrate,rigTemp,rigIP)
 
     f = open("activeips.txt", "w+")
     for x in range(len(active)):
@@ -337,9 +362,8 @@ if __name__ == '__main__':
         f.write(str(active[x]))
         f.write('\n')
     f.close()
-    parseLog()
-    #rigNames = getRigNames()
+    print(rig.getRigAll())
 
-
+    #parseLog()
 
     # set status
